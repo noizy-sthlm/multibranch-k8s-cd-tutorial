bf8abe51e353c03432ffd0f6ab70997e4438b91b
sam <sam.maltin@gmail.com>
2024-09-17 17:11:47 +0200

    test . kubernetes 1

--- killercoda/foreground.sh
+++ killercoda/foreground.sh
@@ -54,6 +54,8 @@
 #!/bin/bash
 
 # Step 1: Wait for the Argo CD server to be ready before starting any actions
+slep 30
+
 echo "Waiting for Argo CD setup to complete..."
 kubectl wait --for=condition=available --timeout=600s -n argocd deploy/argocd-server &>/dev/null
 
@@ -79,6 +81,8 @@ done
 # Step 3: Retrieve the initial admin password for Argo CD
 admin_password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
 
+clear
+
 # Step 4: Display the login credentials to the user
 echo -e "\n\033[1mArgo CD is ready to use.\033[0m"
 echo -e "You can access the Argo CD UI at: \033[1mhttp://localhost:8080\033[0m"
@@ -88,4 +92,6 @@ echo -e "Password: \033[1m$admin_password\033[0m"
 echo -e "\nKeep this terminal open to maintain the port-forwarding session.\n"
 
 # Keep the port-forwarding running
+
+
 wait
