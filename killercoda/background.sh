91a453f730179ea0d550d51b8c643d3617cfa672
sam <sam.maltin@gmail.com>
2024-09-17 16:06:11 +0200

    test . kubernetes

--- killercoda/background.sh
+++ killercoda/background.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Setting up Argo CD in the background
+# Create namespace and install Argo CD
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# Wait for Argo CD to start
+# Wait for Argo CD to be fully deployed
 sleep 60
 
-# Patch Argo CD to allow insecure HTTP
+# Patch the Argo CD deployment to allow insecure HTTP access
 kubectl patch deploy argocd-server -n argocd --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--insecure"}]'
 
-# Restart the Argo CD server
+# Restart the Argo CD deployment to apply the changes
 kubectl rollout restart deploy argocd-server -n argocd
 
-# Sleep to ensure Argo CD is up and running
+# Wait for Argo CD to be fully up
 sleep 30
--- killercoda/foreground.sh
+++ killercoda/foreground.sh
@@ -4,6 +4,9 @@
 echo "Port-forwarding Argo CD on http://localhost:8080"
 kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:80 &
 
+# Wait a bit for port-forwarding to be established
+sleep 5
+
 # Retrieve the initial admin password for Argo CD
 echo "Retrieving Argo CD admin password:"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode
